cd2d1305c41e904c0ebcd530a3fe080c2a50c88d
depaoli91 <92738637+depaoli91@users.noreply.github.com>
2021-11-28 13:49:56 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,1 @@
-var largura = window.screen.width;
-
-function PC(largura){
-  
-  if (largura > 1000) {
-    
-    return window.alert("Hello world!"); 
-    
-}
-    
-  }
+alert("Hello world!"); 
