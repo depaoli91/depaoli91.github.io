f60286adaf237816fe7dc1a86d7e836099b1041b
depaoli91 <92738637+depaoli91@users.noreply.github.com>
2021-11-28 13:31:54 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
-alert("OLA GOSTOSURA, CLIQUE PARA ENTRAR NO SEU SITE")
+var largura = window.screen.width;
+
+new function1(largura){
+  
+  if (largura > 1000 px) {
+    
+    window.location.href = "http://www.devmedia.com.br";
+    
+}
+    
+  }
