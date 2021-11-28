4be343519a6fe3b8cc5d1eb7e1bad532f8ae4d44
depaoli91 <92738637+depaoli91@users.noreply.github.com>
2021-11-28 13:43:20 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ var largura = window.screen.width;
 
 function PC(largura){
   
-  if (largura > 1000 px) {
+  if (largura > 1000) {
     
     return window.location.href = "http://www.devmedia.com.br";
     
