09565e8fd2e22aba33aacfe52428160442c8a3b2
depaoli91 <92738637+depaoli91@users.noreply.github.com>
2021-11-28 15:53:19 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,9 @@
 alert("YV ENGENHARIA 2!"); 
 
+var largura = window.screen.width;
 
-
+    if (largura > 1000) {
+  
+      return window.location.href = "http://www.devmedia.com.br";
+        
+    }
